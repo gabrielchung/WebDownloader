5804e0fc1bef3af24c6c128ae9a760e440f9ade9
Gabriel Chung <gabrielchung1128@gmail.com>
2020-05-07 10:06:44 +1000

    make ready for function call

--- process_url.py
+++ process_url.py
@@ -1,7 +1,6 @@
 from lib import download_webpage as dl
 from lib import parse_webpage as parser
 
-url = 'https://www.nba.com'
-
-url_id = dl.download_url(url, 0)
-parser.parse_url(url_id, 0)
+def process_url(url):
+    url_id = dl.download_url(url, 0)
+    parser.parse_url(url_id, 0)
