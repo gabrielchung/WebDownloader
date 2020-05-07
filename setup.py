b2158397223c5869f9f1873bca406b7ae99e460c
Gabriel Chung <gabrielchung1128@gmail.com>
2020-05-08 09:22:09 +1000

    Adding required packages

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="web-downloader", # Replace with your own username
-    version="0.0.3",
-    author="Gabriel Chung",
-    author_email="gabrielchung1128@gmail.com",
-    description="A Python package using Selenium to download web content",
+    name='web-downloader', # Replace with your own username
+    version='0.0.6',
+    author='Gabriel Chung',
+    author_email='gabrielchung1128@gmail.com',
+    description='A Python package using Selenium to download web content',
     # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    url="https://github.com/gabrielchung/WebDownloader",
+    # long_description_content_type='text/markdown',
+    url='https://github.com/gabrielchung/WebDownloader',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium',
+        'pyautogui',
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Windows',
     ],
     python_requires='>=3.6',
 )
